80cc06beab6295bbc982086691a437019a964d94
Jul <juliedeneys@student.ie.edu>
2018-11-09 18:01:57 +0100

    Bubble Sort
    Added the Bubble Sort Algorithm to the assignment.

---
+++ assignment.py
@@ -0,0 +1,13 @@
+#%%
+
+# Bubble sort implementation.
+#
+# This function returns nothing because it works by mutating the lst.
+def bubble(lst):
+    for j in range(0, len(lst) - 1):
+        for i in range(0, len(lst) - j - 1):
+            if lst[i] > lst[i + 1]:
+                temp = lst[i]
+                lst[i] = lst[i + 1]
+                lst[i + 1] = temp
+                
